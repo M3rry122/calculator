21c18ead40d039c41345c49ee14b806ee07ed085
kohei <ykohei46@outlook.jp>
2025-12-21 22:07:12 +0900

    gi

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 document.getElementById("button1").addEventListener("click", () => {
     const num1 = Number(document.getElementById("num1").value);
     const num2 = Number(document.getElementById("num2").value);
-    const operator = document.getElementById("operator").value;
+    const num3 = Number(document.getElementById("num3").value);
+    const operator1 = document.getElementById("operator1").value;
+    const operator2 = document.getElementById("operator2").value;
 
-    let result;
+    let result1;
+    let result2;
 
-    if (operator === "+") {
-        result = num1 + num2;
-    }   else if (operator === "-") {
-        result = num1 - num2;
-    }   else if (operator === "*") {
-        result = num1 * num2;
-    }   else if (operator === "/") {
-        result = num2 !== 0 ? num1 / num2 : "エラー";
-    }
+    if (operator1 === "+") {
+        result1 = num1 + num2;
+    }   else if (operator1 === "-") {
+        result1 = num1 - num2;
+    }   else if (operator1 === "*") {
+        result1 = num1 * num2;
+    }   else if (operator1 === "/") {
+        result1 = num2 !== 0 ? num1 / num2 : "エラー";
+    };
 
-    document.getElementById("result").textContent = result;
+    if (num3 === null || result1 === "エラー") {
+        result2 = result1;
+    }   else if (operator2 === "+") {
+        result2 = result1 + num3;
+    }   else if (operator2 === "-") {
+        result2 = result1 - num3;
+    }   else if (operator2 === "*") {
+        result2 = result1 * num3;
+    }   else if (operator2 === "/") {
+        result2 = num3 !== 0 ? result1 / num3 : "エラー";
+    };
+
+    document.getElementById("result").textContent = result2;
 })
